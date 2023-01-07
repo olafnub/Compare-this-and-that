e86287160bc164457263b84d0972ce07fb6a5ab3
olafnub <googlhoogle@gmail.com>
2023-01-07 15:41:20 -0600

    Add strings to arrays with spaces in them
    Why - if there were two word ingredients then it would only print it all
    with no spacing, this allows if to be easier to read.

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-let names = "This, and, that"
+let names = "This 12, and 12 and, that"
 let namesArray = [...names];
 let buildWord = "";
 let buildArray = [];
 
 namesArray.forEach((x, i) => {
-	if (x == ",") {
+if (x == ",") {
   buildArray.push(buildWord);
   buildWord = "";
   } 
-  else if (x == " ") {
-  x++;
-  }
-  else {
-  buildWord += x;
-  }
+else if (x == " " && namesArray[i-1] == ",") {
+        x++;
+}
+else {
+    buildWord += x;
+    }
+  
  if (i == namesArray.length-1) {
     buildArray.push(buildWord);
   }
   
   });
+  checkeEach();
+  function checkeEach() {
+    buildArray.forEach(x => {
+        console.log(x);
+    })
+  }
 
-
-console.log(buildArray);
+// console.log(buildArray);

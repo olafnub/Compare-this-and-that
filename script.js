ece83dbaed8b9603321a18aaa95e70d9851f59e2
olafnub <googlhoogle@gmail.com>
2023-01-07 16:03:47 -0600

    Checkpoint before changing the structure

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let names = "This 12, and 12 and, that"
-let namesArray = [...names];
+let string1 = "This 12, and 12 and, that";
+let string1Array = [...string1];
 let buildWord = "";
 let buildArray = [];
 
-namesArray.forEach((x, i) => {
+let array2 = ["this 12, and wa, that12"];
+
+string1Array.forEach((x, i) => {
 if (x == ",") {
   buildArray.push(buildWord);
   buildWord = "";
   } 
-else if (x == " " && namesArray[i-1] == ",") {
+else if (x == " " && string1Array[i-1] == ",") {
         x++;
 }
 else {
     buildWord += x;
     }
   
- if (i == namesArray.length-1) {
+ if (i == string1Array.length-1) {
     buildArray.push(buildWord);
   }
   
@@ -26,5 +28,3 @@ else {
         console.log(x);
     })
   }
-
-// console.log(buildArray);

1b7dacae2b8db9d1b3d2e6a3ba654c41cf49f509
olafnub <googlhoogle@gmail.com>
2023-01-14 11:34:29 -0600

    Create layout of similar matches for ingredients
    Why - Find the similiarities, print a list of similar ingredients with
    break for each line to be able to read better

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-let string1 = "This 12, and 12 and, that";
+let submit = document.getElementById("submit");
+submit.addEventListener("click", checkEach);
+
+let title1;
+let title2;
+
+function checkEach() {
+title1 = document.getElementById("firstInputTitle").value;
+title2 = document.getElementById("secondInputTitle").value;
+
+  returnBuiltArrays(title1, string1Array, buildArray1);
+  returnBuiltArrays(title2, string2Array, buildArray2);
+}
+
+let string1 = "This 12, and 12 and, that, same";
+let string2 = "and Was, bY Then, Straight 43%, same";
 let string1Array = [...string1];
+let string2Array = [...string2];
 let buildWord = "";
+let array1Title = "";
+let array2Title = "";
 let buildArray1 = [];
 let buildArray2 = [];
+let similarMatches = [];
+let lock=0;
 
-let array2 = ["this 12, and wa, that12"];
+function returnBuiltArrays(title, stringArray, buildArray) {
+    for (let i = 0; i <= stringArray.length; i++) {
 
-  for (let i = 0; i <= string1Array.length; i++) {
+    if (stringArray[i] == " " && stringArray[i-1] == ",") {
+      
+    }
+    else if (stringArray[i] != "," && i != stringArray.length) {
+      buildWord += stringArray[i];
+    }
+    else {
+    buildArray.push(buildWord.toUpperCase());
+    buildWord = "";
+    }
+  }
+  lock++;
 
-  if (string1Array[i] == " " && string1Array[i-1] == ",") {
-    
+  if (lock == 2) {
+    compareArray(buildArray1);
   }
-  else if (string1Array[i] != "," && i != string1Array.length) {
-    buildWord += string1Array[i];
-  }
-  else {
-  buildArray.push(buildWord.toUpperCase());
-  buildWord = "";
-  }
-}
+} 
+
+let box1 = document.querySelector("#similarMatches pre");
+let box2 = document.querySelector("#differentMatches pre");
 
 function compareArray() {
-  
+  for (let j = 0; j < buildArray2.length; j++) {
+    buildArray1.filter(n => n===buildArray2[j])
+      .forEach(n => similarMatches.push(" " + n));
+  }
+  box1.textContent = similarMatches;
 }
 
 
-  checkeEach();
-  function checkeEach() {
-    console.log(buildArray);
-  }
